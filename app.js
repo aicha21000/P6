fa104aa57c0020d422c9ef8e1d297ce94ed31c87
aicha21000 <salhi.aicha@hotmail.com>
2023-02-26 16:12:57 +0100

    get method

--- app.js
+++ app.js
@@ -2,6 +2,28 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+  app.use('/api/sauces', (req, res, next) => {
+    const sauce = [
+      {
+        _id: 'oeihfzeoi',
+        
+      },
+      {
+        _id: 'oeihfzeomoihi',
+       
+      },
+    ];
+    res.status(200).json(sauce);
+  });
+
+
 
 
 module.exports = app;
