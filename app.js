e27dd0a5d0c4871060c7a6b8da3c8c1e7cd741ce
aicha21000 <salhi.aicha@hotmail.com>
2023-02-26 16:13:25 +0100

    post method

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -23,7 +24,12 @@ app.use((req, res, next) => {
     res.status(200).json(sauce);
   });
 
-
+  app.post('/api/sauces', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+      message: 'Objet créé !'
+    });
+  });
 
 
 module.exports = app;
